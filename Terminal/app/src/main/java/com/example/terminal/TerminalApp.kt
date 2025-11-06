cf033969078c8487af4d299d4f843a1ef6d76186
reybeld94 <reybeld94@icloud.com>
2025-11-06 15:28:37 -0500

    Fix KeyboardOptions import

--- Terminal/app/src/main/java/com/example/terminal/TerminalApp.kt
+++ Terminal/app/src/main/java/com/example/terminal/TerminalApp.kt
@@ -58,7 +58,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
